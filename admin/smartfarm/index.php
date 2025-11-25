5636aef1617d11dd2fbad6f3a117b11830f6804c
Tansaeng Admin <admin@tansaeng.com>
2025-11-25 09:27:39 +0900

    fix: 빌드 파일명 업데이트로 무한 새로고침 완전 해결 (index-DGb6Ssq2.js)

--- admin/smartfarm/index.php
+++ admin/smartfarm/index.php
@@ -98,7 +98,7 @@ if ($currentUser['email'] !== 'korea_tansaeng@naver.com') {
     $timestamp = time() . rand(1000, 9999);
     ?>
     <div id="root"></div>
-    <script type="module" crossorigin src="/smartfarm-ui/dist/assets/index-IyWIgGQ2.js?v=<?php echo $timestamp; ?>"></script>
-    <link rel="stylesheet" crossorigin href="/smartfarm-ui/dist/assets/index-3qJBwgOj.css?v=<?php echo $timestamp; ?>">
+    <script type="module" crossorigin src="/smartfarm-ui/dist/assets/index-DGb6Ssq2.js?v=<?php echo $timestamp; ?>"></script>
+    <link rel="stylesheet" crossorigin href="/smartfarm-ui/dist/assets/index-BJogyt9j.css?v=<?php echo $timestamp; ?>">
 </body>
 </html>
